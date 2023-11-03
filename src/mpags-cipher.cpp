c882fd34a87eec754bc391bc86078bf638039758
dharmendergaur <112001077+dharmendergaur@users.noreply.github.com>
2023-11-03 21:14:20 +0530

    Update

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -1,156 +1,99 @@
-#include <cctype>
 #include <iostream>
 #include <string>
 #include <vector>
+using namespace std;
 
 int main(int argc, char* argv[])
 {
-    // Convert the command-line arguments into a more easily usable form
-    const std::vector<std::string> cmdLineArgs{argv, argv + argc};
-    const std::size_t nCmdLineArgs{cmdLineArgs.size()};
+    const vector<string> cmdLineArgs {argv, argv+argc };
+    if(argc==1){
+        cout<<"No command line input."<<endl;
+    }
+    bool isKnownArgument = false;
+    const vector<string> knownArguments = {"-h", "-V", "--version", "--help", "-i", "-o"};
 
-    // Options that might be set by the command-line arguments
-    bool helpRequested{false};
-    bool versionRequested{false};
-    std::string inputFile{""};
-    std::string outputFile{""};
 
-    // Process command line arguments - ignore zeroth element, as we know this
-    // to be the program name and don't need to worry about it
-    for (std::size_t i{1}; i < nCmdLineArgs; ++i) {
-        if (cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help") {
-            helpRequested = true;
-        } else if (cmdLineArgs[i] == "--version") {
-            versionRequested = true;
-        } else if (cmdLineArgs[i] == "-i") {
-            // Handle input file option
-            // Next element is filename unless "-i" is the last argument
-            if (i == nCmdLineArgs - 1) {
-                std::cerr << "[error] -i requires a filename argument"
-                          << std::endl;
-                // exit main with non-zero return to indicate failure
-                return 1;
-            } else {
-                // Got filename, so assign value and advance past it
-                inputFile = cmdLineArgs[i + 1];
-                ++i;
-            }
-        } else if (cmdLineArgs[i] == "-o") {
-            // Handle output file option
-            // Next element is filename unless "-o" is the last argument
-            if (i == nCmdLineArgs - 1) {
-                std::cerr << "[error] -o requires a filename argument"
-                          << std::endl;
-                // exit main with non-zero return to indicate failure
-                return 1;
-            } else {
-                // Got filename, so assign value and advance past it
-                outputFile = cmdLineArgs[i + 1];
-                ++i;
-            }
-        } else {
-            // Have an unknown flag to output error message and return non-zero
-            // exit status to indicate failure
-            std::cerr << "[error] unknown argument '" << cmdLineArgs[i]
-                      << "'\n";
-            return 1;
+    for(int i = 0; i < argc; i++){
+        if(cmdLineArgs[i] == "-h" ||cmdLineArgs[i] == "--help"){
+            cout
+                << "Usage: mpags-cipher [-h/--help] [-V/--version] [-i <file>] [-o <file>]\n\n"
+                << "Available options:\n\n"
+                << "  -h or --help        HELP\n"
+                << "  -V or --version     Version information\n"
+                << "  -i <FILE>           Input file to read\n"
+                << "  -o <FILE>           Output file\n"
+                << endl;
+        }
+        if(cmdLineArgs[i] == "-i"){
+            string input_file = cmdLineArgs[i+1];
+            cout << "Input File is: "<< input_file << endl;
+        }
+        if(cmdLineArgs[i] == "-o"){
+            string output_file = cmdLineArgs[i+1];
+            cout << "Output File is: "<< output_file << endl;
+        }
+        if(cmdLineArgs[i] == "-V" || cmdLineArgs[i] == "--version"){
+            cout << "Version: X.Y.Z"<< endl;
+        }
+        for (const string &arg : knownArguments) {
+        if (cmdLineArgs[i] == arg) {
+        isKnownArgument = true;
+         }
         }
     }
-
-    // Handle help, if requested
-    if (helpRequested) {
-        // Line splitting for readability
-        std::cout
-            << "Usage: mpags-cipher [-h/--help] [--version] [-i <file>] [-o <file>]\n\n"
-            << "Encrypts/Decrypts input alphanumeric text using classical ciphers\n\n"
-            << "Available options:\n\n"
-            << "  -h|--help        Print this help message and exit\n\n"
-            << "  --version        Print version information\n\n"
-            << "  -i FILE          Read text to be processed from FILE\n"
-            << "                   Stdin will be used if not supplied\n\n"
-            << "  -o FILE          Write processed text to FILE\n"
-            << "                   Stdout will be used if not supplied\n\n"
-            << std::endl;
-        // Help requires no further action, so return from main
-        // with 0 used to indicate success
-        return 0;
+    if (!isKnownArgument && argc > 1) {
+    cout << "\033[31m Unknown Argument.\033[0m" << endl;
     }
 
-    // Handle version, if requested
-    // Like help, requires no further action,
-    // so return from main with zero to indicate success
-    if (versionRequested) {
-        std::cout << "0.1.0" << std::endl;
-        return 0;
+    char in_char{'x'};
+  string out_str{""};
+cout << "\033[31m Enter Input: \033[0m" << endl;
+  // Take letter from input:
+  while(cin>>in_char)
+  {
+    if(isalpha(in_char))
+    {
+      out_str+=toupper(in_char);
+      continue;
     }
 
-    // Initialise variables
-    char inputChar{'x'};
-    std::string inputText;
-
-    // Read in user input from stdin/file
-    // Warn that input file option not yet implemented
-    if (!inputFile.empty()) {
-        std::cerr << "[warning] input from file ('" << inputFile
-                  << "') not implemented yet, using stdin\n";
+    switch (in_char)
+    {
+    case '0':
+      out_str+="ZERO";
+      break;
+    case '1':
+      out_str+="ONE";
+      break;
+    case '2':
+      out_str+="TWO";
+      break;
+    case '3':
+      out_str+="THREE";
+      break;
+    case '4':
+      out_str+="FOUR";
+      break;
+    case '5':
+      out_str+="FIVE";
+      break;
+    case '6':
+      out_str+="SIX";
+      break;
+    case '7':
+      out_str+="SEVEN";
+      break;
+    case '8':
+      out_str+="EIGHT";
+      break;
+    case '9':
+      out_str+="NINE";
+      break;
+    default:
+      break;
     }
+  }
+  cout << out_str << endl;
 
-    // loop over each character from user input
-    while (std::cin >> inputChar) {
-        // Uppercase alphabetic characters
-        if (std::isalpha(inputChar)) {
-            inputText += std::toupper(inputChar);
-            continue;
-        }
-
-        // Transliterate digits to English words
-        switch (inputChar) {
-            case '0':
-                inputText += "ZERO";
-                break;
-            case '1':
-                inputText += "ONE";
-                break;
-            case '2':
-                inputText += "TWO";
-                break;
-            case '3':
-                inputText += "THREE";
-                break;
-            case '4':
-                inputText += "FOUR";
-                break;
-            case '5':
-                inputText += "FIVE";
-                break;
-            case '6':
-                inputText += "SIX";
-                break;
-            case '7':
-                inputText += "SEVEN";
-                break;
-            case '8':
-                inputText += "EIGHT";
-                break;
-            case '9':
-                inputText += "NINE";
-                break;
-        }
-
-        // If the character isn't alphabetic or numeric, DONT add it
-    }
-
-    // Print out the transliterated text
-
-    // Warn that output file option not yet implemented
-    if (!outputFile.empty()) {
-        std::cerr << "[warning] output to file ('" << outputFile
-                  << "') not implemented yet, using stdout\n";
-    }
-
-    std::cout << inputText << std::endl;
-
-    // No requirement to return from main, but we do so for clarity
-    // and for consistency with other functions
     return 0;
 }
